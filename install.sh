c2d31380b3e43f954e5310fa731c06d76e294ae2
Patrice Ferlet <metal3d@gmail.com>
2017-11-19 18:26:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,9 @@ select_path(){
 
 which curl 2>&1 >/dev/null || echo "You need curl to complete instalation"
 select_path
-
-curl -sSL -# \
+echo "Installing in $LOCATION"
+curl -sSL \
     $(curl -s https://api.github.com/repos/metal3d/goswitch/releases/latest | grep tarball | cut -d '"' -f 4) | \
     tar -zxf - --strip-components=1 -C $LOCATION \*/goswitch
+
+echo 'Done, try "goswitch version" command'
