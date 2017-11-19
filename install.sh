3d2a3f2c72dd353a1420aa55202931f8f5cc9542
Patrice Ferlet <metal3d@gmail.com>
2017-11-19 18:21:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
-curl -sSL $(curl -s https://api.github.com/repos/metal3d/goswitch/releases/latest | \
-    grep tarball | cut -d '"' -f 4) | \
-    tar -zxf - --strip-components=1 -C ~/.local/bin */goswitch
+LOCATION=""
+select_path(){
+    local OLDIFS=$IFS
+    local IFS=":"
+    location=""
+
+    P=()
+    for p in ${PATH[*]}; do
+        P+=( "$p" )
+    done
+
+    P=$(local IFS=$'\n'; sort -u <<<"${P[*]}" | uniq)
+    IFS=$OLDIFS
+
+    echo "Select a directory that is in your PATH."
+    echo "Prefer $HOME/.local/bin or $HOME/bin if they are in list please." 
+    select place in "Cancel" ${P} ; do
+        if [ "$place" == "Cancel" ]; then
+            exit 0
+        fi
+        location=$place
+        if [ "$location" == "" ]; then
+            echo "Please select a valid option"
+            continue
+        fi
+        break
+    done
+    LOCATION="$location"
+}
+
+which curl 2>&1 >/dev/null || echo "You need curl to complete instalation"
+select_path
+
+curl -sSL \
+    $(curl -s https://api.github.com/repos/metal3d/goswitch/releases/latest | grep tarball | cut -d '"' -f 4) | \
+    tar -zxf - --strip-components=1 -C $LOCATION \*/goswitch
